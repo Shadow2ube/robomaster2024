19d1b8d59771e1e0f0c36653edb8ab08e69e119a
Chritsian LaForest <christianlaforest45@gmail.com>
2024-05-15 16:03:21 -0400

    forcing RUN through bash so we can run a source script

---
+++ detect_v2/ws/build/detect/CMakeFiles/detect_node.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/detect_node.dir/src/main.cpp.o"
+  "/ws/devel/.private/detect/lib/detect/detect_node.pdb"
+  "/ws/devel/.private/detect/lib/detect/detect_node"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/detect_node.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()

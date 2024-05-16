b76a847aea3475c5c0967aa7da300c6bd838b5c6
Chritsian LaForest <christianlaforest45@gmail.com>
2024-05-15 21:42:46 -0400

    error

--- detect_v2/ws/src/detect/src/main.cpp
+++ detect_v2/ws/src/detect/src/main.cpp
@@ -29,11 +29,11 @@ geometry_msgs::Pose make_pose(float x, float y, float z, float ax, float ay, flo
   return p;
 }
 
-void detect_callback(sensor_msgs::Image) {
+void detect_callback(sensor_msgs::Image img) {
   ROS_INFO("got an image");
   cv_bridge::CvImagePtr cv_ptr;
   try {
-    cv_ptr = cv_bridge::toCvCopy(msg, sensor_msgs::image_encodings::BGR8);
+    cv_ptr = cv_bridge::toCvCopy(img, sensor_msgs::image_encodings::BGR8);
   }
   catch (cv_bridge::Exception& e) {
     ROS_ERROR("cv_bridge exception: %s", e.what());

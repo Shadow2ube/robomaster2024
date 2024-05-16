476826ff69c6b3a28d022ad7d8c59b88fde47c73
Chritsian LaForest <christianlaforest45@gmail.com>
2024-05-15 21:40:40 -0400

    continuing the conversion

--- detect_v2/ws/src/detect/src/main.cpp
+++ detect_v2/ws/src/detect/src/main.cpp
@@ -12,6 +12,11 @@
 
 #include <cv_bridge/cv_bridge.h>
 
+
+ros::Publisher target_pub;
+ros::Publisher found_pub;
+ros::Publisher img_pub;
+
 geometry_msgs::Pose make_pose(float x, float y, float z, float ax, float ay, float az, float aw = 0) {
   auto p = geometry_msgs::Pose();
   p.position.x = x;
@@ -26,6 +31,16 @@ geometry_msgs::Pose make_pose(float x, float y, float z, float ax, float ay, flo
 
 void detect_callback(sensor_msgs::Image) {
   ROS_INFO("got an image");
+  cv_bridge::CvImagePtr cv_ptr;
+  try {
+    cv_ptr = cv_bridge::toCvCopy(msg, sensor_msgs::image_encodings::BGR8);
+  }
+  catch (cv_bridge::Exception& e) {
+    ROS_ERROR("cv_bridge exception: %s", e.what());
+    return;
+  }
+  
+  img_pub.publish(cv_ptr->toImageMsg());
 }
 
 int main(int argc, char **argv) {
@@ -48,9 +63,9 @@ int main(int argc, char **argv) {
   ros::init(argc, argv, "detect");
   ros::NodeHandle n;
 
-  ros::Publisher target_pub = n.advertise<geometry_msgs::Pose>("found/target", 10);
-  ros::Publisher found_pub = n.advertise<geometry_msgs::PoseArray>("found/all", 10);
-  ros::Publisher img_pub = n.advertise<geometry_msgs::PoseArray>("camera/detection", 10);
+  target_pub = n.advertise<geometry_msgs::Pose>("found/target", 10);
+  found_pub = n.advertise<geometry_msgs::PoseArray>("found/all", 10);
+  img_pub = n.advertise<geometry_msgs::PoseArray>("camera/detection", 10);
 
   ros::Subscriber image_sub = n.subscribe(outpath, 10, detect_callback);
 
